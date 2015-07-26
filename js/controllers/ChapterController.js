f60f4422447c817f82ce7f67ae73c29de0fb39bf
dchen71 <dchen12812@gmail.com>
2015-07-26 16:09:40 -0700

    implemented solution

--- js/controllers/ChapterController.js
+++ js/controllers/ChapterController.js
@@ -1,17 +1,22 @@
 app.controller('ChapterController', ['$scope', 'books', '$routeParams', function($scope, books, $routeParams) {
   books.success(function(data) {
     // Your code here
-
-
-
-    // If there no more chapters left, go back to the bookshelf view
-    if($routeParams.chapterId >= $scope.book.chapters.length - 1) {
-      $scope.nextChapterIndex = "#";
-    }
-  });
+  $scope.book =  data[$routeParams.bookId];
+  $scope.chapter = $scope.book.chapters[$routeParams.chapterId];
 
   // Using these properties to create the URLs in line 1 and line 11 of view/chapter.html
   $scope.currentBookIndex = parseInt($routeParams.bookId);
   $scope.currentChapterIndex = parseInt($routeParams.chapterId);
   $scope.nextChapterIndex = $scope.currentChapterIndex + 1;
+
+      // If there no more chapters left, go back to the bookshelf view
+
+
+  console.log($scope.book.chapters.length)  
+  if($routeParams.chapterId >= $scope.book.chapters.length - 1) {
+
+      $scope.nextChapterIndex = "#";
+    }
+  });
+
 }]);
